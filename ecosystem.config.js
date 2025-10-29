37c9aa99e61a4ca922baae76b437c975ca17eaad
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2025-10-29 16:43:24 +0100

    fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,41 +5,23 @@
  *   pm2 restart ecosystem.config.js
  */
 module.exports = {
-  apps: [
+  "apps": [
     {
-      name: 'trashbin-scheduler-bot',
-      script: './trashbin-day-handler.js',
-      cwd: __dirname,
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_restarts: 10,
-      env: {
-        NODE_ENV: 'production',
-        // Fill with your bot token (should be kept secret). You can also use a .env file.
-        BOT_TOKEN: '',
-        // Comma-separated list of chat ids to send startup/scheduled messages to
-        TARGET_CHAT_ID: '',
-        // Comma-separated list of admin chat ids allowed to run admin commands
-        ADMIN_CHAT_ID: '7328814364',
-        // When 'true', scheduled messages will be sent to all stored subscribers (db/chats.json)
-        SEND_TO_SUBSCRIBERS: 'false',
-        // When 'true', append request logs to logs/requests.log in project root
-        WRITE_REQUESTS_LOG: 'false'
+      "name": "trashbin-scheduler-bot",
+      "script": "./trashbin-day-handler.js",
+      "cwd": "/Users/danilo/GitHub/trashbin-scheduler-bot-tg",
+      "instances": 1,
+      "autorestart": true,
+      "watch": false,
+      "max_restarts": 10,
+      "env": {
+        "NODE_ENV": "production",
+        "BOT_TOKEN": "8442580531:AAHzgUxXCxWNif9K_mA3dT8kxgyR9LCeXXE",
+        "TARGET_CHAT_ID": "7328814364",
+        "ADMIN_CHAT_ID": "7328814364",
+        "SEND_TO_SUBSCRIBERS": "false",
+        "WRITE_REQUESTS_LOG": "false"
       }
     }
   ]
-};
-module.exports = {
-  apps: [{
-    name: 'trashbin-bot',
-    script: './trashbin-day-handler.js',
-    cwd: './trashbin-scheduler-bot-tg',
-    env: {
-      NODE_ENV: 'production',
-      TARGET_CHAT_ID: '', // opzionale
-      BOT_TOKEN: '8442580531:AAHzgUxXCxWNif9K_mA3dT8kxgyR9LCeXXE'
-      // Non mettere BOT_TOKEN qui se preferisci tenerlo in .env
-    }
-  }]
 };
